afdf3761826472662b9191a94e3ad927ba0b9922
MichaelR139 <michaelradvinsky@gmail.com>
2022-05-14 09:10:07 -0700

    2st commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,12 @@
 package com.company;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Main {
+    public void Main(){
+        System.out.println("gggg");
+    }
+
+
+
 
 }
